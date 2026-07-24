b5f4d1f61b18d8e061ee5145ac8afe7b289d257e
Ana Clara Setulim Silva <aclara.ssilva@gmail.com>
2026-07-24 14:58:49 -0300

    Create #C.py

---
+++ MFP 2023/#C.py
@@ -0,0 +1,21 @@
+d, m = map(int, input().split())
+
+tam_corda = d/((2**0.5)/2) #tam do segmento da corda de um prédio até outro
+seg_completo = m//tam_corda 
+seg_sobra = m%tam_corda
+
+x = m*((2**0.5)/2)
+
+if d == 0:
+    y = 0
+elif seg_sobra==0 and seg_completo%2==0:
+    y = 0
+elif seg_sobra==0 and seg_completo%2==1:
+    y = d
+elif seg_sobra!=0 and seg_completo%2==0: #qntd par de seg, amy ta subindo
+    y = seg_sobra * ((2**0.5)/2)
+elif seg_sobra!=0 and seg_completo%2==1: #qntd ímpar de seg, amy ta descendo
+    y = d - (seg_sobra * ((2**0.5)/2))
+
+print(f'{x:.6f} {y:.6f}')
+
