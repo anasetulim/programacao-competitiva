75fe55844b30dd3b55179f454014d100a395fedc
Ana Clara Setulim Silva <aclara.ssilva@gmail.com>
2026-07-24 15:00:46 -0300

    treino

--- MFP 2023/#C.py
+++ MFP 2023/#C.py
@@ -12,9 +12,9 @@ elif seg_sobra==0 and seg_completo%2==0:
     y = 0
 elif seg_sobra==0 and seg_completo%2==1:
     y = d
-elif seg_sobra!=0 and seg_completo%2==0: #qntd par de seg, amy ta subindo
+elif seg_sobra!=0 and seg_completo%2==0: #qntd par de seg, amy ta subindo no eixo y
     y = seg_sobra * ((2**0.5)/2)
-elif seg_sobra!=0 and seg_completo%2==1: #qntd ímpar de seg, amy ta descendo
+elif seg_sobra!=0 and seg_completo%2==1: #qntd ímpar de seg, amy ta descendo no eixo y
     y = d - (seg_sobra * ((2**0.5)/2))
 
 print(f'{x:.6f} {y:.6f}')
