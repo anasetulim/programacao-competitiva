efca91cf3d6ab758d0e0e4c82ddc8da36086687a
Ana Clara Setulim Silva <aclara.ssilva@gmail.com>
2026-07-24 13:48:55 -0300

    treino

--- MFP 2023/#B.py
+++ MFP 2023/#B.py
@@ -5,7 +5,7 @@ maior_planta = 0
 h.sort()
 
 for i in range(n):
-    altura_planta = ((h[i]) + h[n-1-i])/2 #compensando a altura fazendo a média entre a menor e a maior planta
+    altura_planta = ((h[i]) + h[n-1-i])//2 #compensando a altura fazendo a média entre a menor e a maior planta
     if altura_planta > maior_planta:
         maior_planta = altura_planta
 
